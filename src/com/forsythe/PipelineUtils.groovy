0e17f7f2884e74fe4b22c600041be25b11a68980
akalinovski <a.kalinovski@gmail.com>
2017-12-06 16:49:19 +0300

    Added initial commands for pipelines

--- src/com/forsythe/PipelineUtils.groovy
+++ src/com/forsythe/PipelineUtils.groovy
@@ -4,6 +4,9 @@ import com.cloudbees.groovy.cps.NonCPS
 
 class PipelineUtils implements Serializable {
 
+    def steps
+    PipelineUtils(steps) {this.steps = steps}
+
     @NonCPS
     def extractNamespace(jobName) {
         return jobName.tokenize('/')[0]
@@ -17,48 +20,52 @@ class PipelineUtils implements Serializable {
 
     @NonCPS
     def waitForAllPodsRunning(String namespace) {
-        while (true) {
-            podsStatus = sh(returnStdout: true, script: "kubectl --namespace='${namespace}' get pods --no-headers").trim()
-            def notRunning = podsStatus.readLines().findAll { line -> !line.contains('Running') }
-            if (notRunning.isEmpty()) {
-                echo 'All pods are running'
-                break
+        steps.timeout(time: 3, unit: 'MINUTES') {
+            while (true) {
+                podsStatus = steps.sh(returnStdout: true, script: "kubectl --namespace='${namespace}' get pods --no-headers").trim()
+                def notRunning = podsStatus.readLines().findAll { line -> !line.contains('Running') }
+                if (notRunning.isEmpty()) {
+                    print 'All pods are running'
+                    break
+                }
+                steps.sh "kubectl --namespace='${namespace}' get pods"
+                steps.sleep 10
             }
-            sh "kubectl --namespace='${namespace}' get pods"
-            sleep 10
         }
     }
 
     @NonCPS
     def waitForAllServicesRunning(String namespace) {
-        while (true) {
-            servicesStatus = sh(returnStdout: true, script: "kubectl --namespace='${namespace}' get services --no-headers").trim()
-            def notRunning = servicesStatus.readLines().findAll { line -> line.contains('pending') }
-            if (notRunning.isEmpty()) {
-                echo 'All pods are running'
-                break
+        steps.timeout(time: 3, unit: 'MINUTES') {
+            while (true) {
+                servicesStatus = steps.sh(returnStdout: true, script: "kubectl --namespace='${namespace}' get services --no-headers").trim()
+                def notRunning = servicesStatus.readLines().findAll { line -> line.contains('pending') }
+                if (notRunning.isEmpty()) {
+                    print 'All pods are running'
+                    break
+                }
+                steps.sh "kubectl --namespace='${namespace}' get services"
+                steps.sleep 10
             }
-            sh "kubectl --namespace='${namespace}' get services"
-            sleep 10
         }
     }
 
     @NonCPS
     def createNamespace(String namespace) {
         try {
-            sh "kubectl create namespace ${projectNamespace} || true"
+            steps.sh "kubectl create namespace ${projectNamespace} || true"
         } catch (Exception ex) {
         }
     }
 
     @NonCPS
     def deleteNamespace(String namespace) {
-        sh "kubectl delete namespace ${projectNamespace} --ignore-not-found=true"
+        steps.sh "kubectl delete namespace ${projectNamespace} --ignore-not-found=true"
     }
 
     @NonCPS
     def extractServiceEndpoint(String namespace, String serviceName) {
-        nexusEndpoint = sh(returnStdout: true, script: "kubectl --namespace='${namespace}' get svc ${serviceName} --no-headers --template '{{ range (index .status.loadBalancer.ingress 0) }}{{ . }}{{ end }}'").trim()
+        nexusEndpoint = steps.sh(returnStdout: true, script: "kubectl --namespace='${namespace}' get svc ${serviceName} --no-headers --template '{{ range (index .status.loadBalancer.ingress 0) }}{{ . }}{{ end }}'").trim()
     }
 
 }
