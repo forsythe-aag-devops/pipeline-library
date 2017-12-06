3894292479ac434384ec4bf50ca73a80830f55a4
akalinovski <a.kalinovski@gmail.com>
2017-12-06 16:30:10 +0300

    Added initial commands for pipelines

--- src/com/forsythe/PipelineUtils.groovy
+++ src/com/forsythe/PipelineUtils.groovy
@@ -1,5 +1,6 @@
 #!/usr/bin/groovy
 package com.forsythe
+import com.cloudbees.groovy.cps.NonCPS
 
 class PipelineUtils implements Serializable {
 
@@ -16,33 +17,29 @@ class PipelineUtils implements Serializable {
 
     @NonCPS
     def waitForAllPodsRunning(String namespace) {
-        timeout(time: 3, unit: 'MINUTES') {
-            while (true) {
-                podsStatus = sh(returnStdout: true, script: "kubectl --namespace='${namespace}' get pods --no-headers").trim()
-                def notRunning = podsStatus.readLines().findAll { line -> !line.contains('Running') }
-                if (notRunning.isEmpty()) {
-                    echo 'All pods are running'
-                    break
-                }
-                sh "kubectl --namespace='${namespace}' get pods"
-                sleep 10
+        while (true) {
+            podsStatus = sh(returnStdout: true, script: "kubectl --namespace='${namespace}' get pods --no-headers").trim()
+            def notRunning = podsStatus.readLines().findAll { line -> !line.contains('Running') }
+            if (notRunning.isEmpty()) {
+                echo 'All pods are running'
+                break
             }
+            sh "kubectl --namespace='${namespace}' get pods"
+            sleep 10
         }
     }
 
     @NonCPS
     def waitForAllServicesRunning(String namespace) {
-        timeout(time: 3, unit: 'MINUTES') {
-            while (true) {
-                servicesStatus = sh(returnStdout: true, script: "kubectl --namespace='${namespace}' get services --no-headers").trim()
-                def notRunning = servicesStatus.readLines().findAll { line -> line.contains('pending') }
-                if (notRunning.isEmpty()) {
-                    echo 'All pods are running'
-                    break
-                }
-                sh "kubectl --namespace='${namespace}' get services"
-                sleep 10
+        while (true) {
+            servicesStatus = sh(returnStdout: true, script: "kubectl --namespace='${namespace}' get services --no-headers").trim()
+            def notRunning = servicesStatus.readLines().findAll { line -> line.contains('pending') }
+            if (notRunning.isEmpty()) {
+                echo 'All pods are running'
+                break
             }
+            sh "kubectl --namespace='${namespace}' get services"
+            sleep 10
         }
     }
 
