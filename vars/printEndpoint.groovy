978e97de9e6f3a2dd088c331ccfdab18ad5b7c5b
akalinovski <a.kalinovski@gmail.com>
2017-12-07 14:45:41 +0300

    Added initial commands for pipelines

--- vars/printEndpoint.groovy
+++ vars/printEndpoint.groovy
@@ -2,8 +2,10 @@
 def call(body) {
     // evaluate the body block, and collect configuration into the object
     def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
+
     def namespace = config.namespace ?: "default"
     def serviceName = config.serviceName ?: "MicroService"
     def serviceId = config.serviceId
