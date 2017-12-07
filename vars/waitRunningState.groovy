1b919c3a93845a39375fae8cadd50fc7e3371213
akalinovski <a.kalinovski@gmail.com>
2017-12-07 11:53:37 +0300

    Added initial commands for pipelines

---
+++ vars/waitRunningState.groovy
@@ -0,0 +1,38 @@
+#!/usr/bin/groovy
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.delegate = config
+    body()
+    def namespace = config.namespace ?: "default";
+
+    try {
+        timeout(time: 10, unit: 'MINUTES') {
+            while (true) {
+                def podsStatus = sh(returnStdout: true, script: "kubectl --namespace='${namespace}' get pods --no-headers").trim()
+                def notRunning = podsStatus.readLines().findAll { line -> !line.contains('Running') }
+                if (notRunning.isEmpty()) {
+                    echo 'All pods are running'
+                    break
+                }
+                sh "kubectl --namespace='${namespace}' get pods"
+                sleep 10
+            }
+
+            while (true) {
+                def servicesStatus = sh(returnStdout: true, script: "kubectl --namespace='${namespace}' get services --no-headers").trim()
+                def notRunning = servicesStatus.readLines().findAll { line -> line.contains('pending') }
+                if (notRunning.isEmpty()) {
+                    echo 'All pods are running'
+                    break
+                }
+                sh "kubectl --namespace='${namespace}' get services"
+                sleep 10
+            }
+        }
+    } catch (err) {
+        echo "The cluster is in wrong state:"
+        echo "Exception: ${err}"
+        throw err
+    }
+}
