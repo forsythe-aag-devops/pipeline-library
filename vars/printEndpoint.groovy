5b71d7f0d3db6b51e5e9f12d9c78c107fa44adf9
akalinovski <a.kalinovski@gmail.com>
2017-12-07 15:14:29 +0300

    Added initial commands for pipelines

--- vars/printEndpoint.groovy
+++ vars/printEndpoint.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/groovy
-def call(config) {
-    def namespace = config.namespace ?: "default"
-    def serviceName = config.serviceName ?: "MicroService"
+def call(Map config) {
+    def namespace = config.namespace
+    def serviceName = config.serviceName
     def serviceId = config.serviceId
-    def servicePort = config.port ?: "8080"
+    def servicePort = config.port
 
     try {
         def serviceEndpoint = sh(returnStdout: true, script: "kubectl --namespace='${namespace}' get svc ${serviceId} --no-headers --template '{{ range (index .status.loadBalancer.ingress 0) }}{{ . }}{{ end }}'").trim()
