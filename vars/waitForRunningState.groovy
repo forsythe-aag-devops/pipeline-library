95b23b9cd5a3f82093feea87bb5ae005f7d796cd
akalinovski <a.kalinovski@gmail.com>
2017-12-07 14:53:00 +0300

    Added initial commands for pipelines

--- vars/waitForRunningState.groovy
+++ vars/waitForRunningState.groovy
@@ -1,11 +1,5 @@
 #!/usr/bin/groovy
-def call(body) {
-    // evaluate the body block, and collect configuration into the object
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-    def namespace = config.namespace ?: "default";
+def call(String namespace) {
 
     try {
         timeout(time: 10, unit: 'MINUTES') {
